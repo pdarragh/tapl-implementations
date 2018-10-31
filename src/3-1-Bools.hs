1c685b4959b448ffea887c0761a47a960e29978b
Pierce Darragh <pierce.darragh@gmail.com>
2018-10-31 15:10:51 -0600

    Rewrite Bools to use proper small-step evaluation

--- src/3-1-Bools.hs
+++ src/3-1-Bools.hs
@@ -2,6 +2,11 @@ module Bools where
 
 {- Implementation of Figure 3-1: Booleans (p. 34). -}
 
+-- This definition allows monadic small-step evaluation without headaches.
+(<#>) :: Applicative f => f (a -> b) -> a -> f b
+(<#>) func x = func <*> pure x
+infixl 4 <#>
+
 data Term
     = TrueTerm
     | FalseTerm
@@ -9,14 +14,12 @@ data Term
     deriving (Show)
 
 eval' :: Term -> Maybe Term
--- E-IfTrue
-eval' (IfTerm TrueTerm t _) = Just t
--- E-IfFalse
-eval' (IfTerm FalseTerm _ t) = Just t
--- E-If
-eval' (IfTerm t1 t2 t3) = Just (IfTerm (eval t1) t2 t3)
--- (Nothing matches.)
-eval' _ = Nothing
+eval' t =
+    case t of
+        IfTerm TrueTerm  t  _  -> return t
+        IfTerm FalseTerm _  t  -> return t
+        IfTerm t1        t2 t3 -> IfTerm <$> eval' t1 <#> t2 <#> t3
+        _ -> Nothing
 
 eval :: Term -> Term
 eval t = case (eval' t) of
