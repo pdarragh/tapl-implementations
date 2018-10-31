69ad8d63325e60d87cc284a5e87c9ed7b1fa1ce4
Pierce Darragh <pierce.darragh@gmail.com>
2018-10-30 22:22:56 -0600

    Add comments to name evaluation rules

--- src/3-1-Bools.hs
+++ src/3-1-Bools.hs
@@ -9,9 +9,13 @@ data Term
     deriving (Show)
 
 eval' :: Term -> Maybe Term
+-- E-IfTrue
 eval' (IfTerm TrueTerm t _) = Just t
-eval' (IfTerm FalseTerm _ f) = Just f
-eval' (IfTerm c t f) = Just (IfTerm (eval c) t f)
+-- E-IfFalse
+eval' (IfTerm FalseTerm _ t) = Just t
+-- E-If
+eval' (IfTerm t1 t2 t3) = Just (IfTerm (eval t1) t2 t3)
+-- (Nothing matches.)
 eval' _ = Nothing
 
 eval :: Term -> Term
