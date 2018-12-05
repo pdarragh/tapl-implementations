bebd3a17a11313269f6ee5c200c34aba2874122d
Pierce Darragh <pierce.darragh@gmail.com>
2018-12-05 11:17:45 -0700

    Implement evaluation for type arithmetic expressions

--- src/8-2-TyArith.hs
+++ src/8-2-TyArith.hs
@@ -47,3 +47,42 @@ typeOf t =
         SuccTerm t'     -> checkType t' NatType *> return NatType
         PredTerm t'     -> checkType t' NatType *> return NatType
         IsZeroTerm t'   -> checkType t' NatType *> return BoolType
+
+isNumericVal :: Term -> Bool
+isNumericVal t =
+    case t of
+        ZeroTerm    -> True
+        SuccTerm t' -> isNumericVal t'
+        PredTerm t' -> isNumericVal t'
+        _           -> False
+
+isVal :: Term -> Bool
+isVal t =
+    case t of
+        TrueTerm                -> True
+        FalseTerm               -> True
+        t' | isNumericVal t'    -> True
+        _                       -> False
+
+eval' :: Term -> Maybe Term
+eval' t =
+    case t of
+        IfTerm TrueTerm t2 _        -> return t2
+        IfTerm FalseTerm _ t3       -> return t3
+        IfTerm t1 t2 t3             -> IfTerm <$> eval' t1 <#> t2 <#> t3
+        SuccTerm t'                 -> SuccTerm <$> eval' t'
+        PredTerm ZeroTerm           -> return ZeroTerm
+        PredTerm (SuccTerm nv)
+            | isNumericVal nv       -> return nv
+        PredTerm t'                 -> PredTerm <$> eval' t'
+        IsZeroTerm ZeroTerm         -> return TrueTerm
+        IsZeroTerm (SuccTerm nv)
+            | isNumericVal nv       -> return FalseTerm
+        IsZeroTerm t'               -> IsZeroTerm <$> eval' t'
+        _                           -> Nothing
+
+eval :: Term -> Term
+eval t =
+    case (eval' t) of
+        Just t' -> eval t'
+        Nothing -> t
